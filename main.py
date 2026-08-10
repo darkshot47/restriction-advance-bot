d234b8fd20390110950c87092b3d72cf8f861651
darkshot47 <yadavshiva7084@gmail.com>
2026-08-10 16:19:53 +0530

    Simplify bot execution method
    Refactored bot execution to use bot.run() instead of asyncio event loop.

--- main.py
+++ main.py
@@ -611,9 +611,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(main())
-    finally:
-        loop.close()
+    bot.run()
