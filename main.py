cfb5f76a462a5e537a5297b6e29e68413e1b285d
darkshot47 <yadavshiva7084@gmail.com>
2026-09-26 13:01:36 +0530

    Fix indentation for return statement in main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ async def try_native_copy(message, fetch_client, chat_target, msg_id):
 
     except Exception as e:
         print(f"[COPY FAILED] {type(e).__name__}: {e}")
-            return False
+        return False
 async def fetch_and_send(message, status, fetch_client, chat_target, msg_id):
     user_id = message.from_user.id
     file_path = None
