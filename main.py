2983f0d57a49477da8c99f7c80a8ff7ea1e3795a
darkshot47 <yadavshiva7084@gmail.com>
2026-09-26 13:24:59 +0530

    Fix indentation for copy_message parameters

--- main.py
+++ main.py
@@ -177,10 +177,10 @@ async def try_native_copy(message, fetch_client, chat_target, msg_id):
 
         msg = await fetch_client.get_messages(chat_target, msg_id) 
         await fetch_client.copy_message(
-        chat_id=message.chat.id,
-        from_chat_id=msg.chat.id,
-        message_id=msg.id
-    )
+            chat_id=message.chat.id,
+            from_chat_id=msg.chat.id,
+            message_id=msg.id
+        )
 
         return True
 
